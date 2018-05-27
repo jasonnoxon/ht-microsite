6d798ac9121b7fb25d6711f3527ceaa66419e1fe
jason-noxon <jason@palindromecreative.com>
2018-05-27 11:14:55 -0400

    Added copying of html files to dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-  sass: 'src/scss/*.scss'
+  sass: 'src/scss/*.scss',
+  html: 'src/html/*.html'
 }
 
 var APPPATHS = {
@@ -16,8 +18,14 @@ var APPPATHS = {
 
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATHS.sass)
+  .pipe(autoprefixer())
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
   .pipe(gulp.dest(APPPATHS.css))
+});
+
+gulp.task('copy', function(){
+  gulp.src(SOURCEPATHS.html)
+      .pipe(gulp.dest(APPPATHS.root))
 });
 
 gulp.task('serve', ['sass'], function(){
@@ -28,8 +36,9 @@ gulp.task('serve', ['sass'], function(){
   })
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
+gulp.task('watch', ['serve', 'sass', 'copy'], function(){
   gulp.watch([SOURCEPATHS.sass], ['sass']);
+  gulp.watch([SOURCEPATHS.html], ['copy']);
 });
 
 gulp.task('default', ['watch']);
