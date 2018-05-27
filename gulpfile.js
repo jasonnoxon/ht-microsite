e30490bab787916e8167aa9bad29f2ce4fa7f964
jason-noxon <jason@palindromecreative.com>
2018-05-27 08:48:08 -0400

    Added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var SOURCEPATHS = {
+  sass: 'src/scss/*.scss'
+}
+
+var APPPATHS = {
+  root: 'dist/',
+  css: 'dist/css',
+  js: 'dist/js',
+  img: 'dist/img'
+}
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sass)
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-  .pipe(gulp.dest('app/css'))
+  .pipe(gulp.dest(APPPATHS.css))
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function(){
+  browserSync.init([APPPATHS.css + '/*.css', APPPATHS.root + '/*.html', APPPATHS.js + '/*.js'], {
+    server: {
+      baseDir: APPPATHS.root
+    }
+  })
+});
+
+gulp.task('default', ['serve']);
