a1050e05bc8d41efb17bc704625861b4ee683fe5
Jason Noxon <jason.noxon@abhealth.us>
2018-05-28 20:49:34 -0400

    Added Healthy Together Specific content to framework

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,48 @@
 global.jQuery = require('jquery');
 popper = require('popper.js');
 bootstrap = require('bootstrap');
-mustache = require('mustache');
+Vue = require('vue');
+axios = require('axios');
 
-jQuery(document).ready( function($) {
-  var jqhxr = $.getJSON('data.json', function(){
+var url = 'https://api.uniteus.io/networks/f022b5dd-e985-44ab-810a-ef0bd8fd7765/groups?access_token=2ZZ4-kHMgYUx8TGDZ_6OFkWC_r66qSwOWxyhCubH'
+var app = new Vue({
+  el: '#app',
+  created() {
+         this.fetchData();
+     },
+     data: {
+         partners: [],
+         active_partner: [{ name: "none", description: "none", website_url: "none", logo_url: "none"}]
+     },
+     methods: {
+         fetchData() {
+           axios.get(url).then(response => {
+             this.partners = response.data.data;
 
-  }).done( function(data){
-    var template = $('#template').html();
-    var showTemplate = mustache.render(template, data);
-    $('#gallery').html(showTemplate)
-  });
-
+             this.active_partner = this.partners[0];
+           })
+         },
+         setActivePartner(id) {
+           var i = this.partners.map(item => item.id).indexOf(id);
+           this.active_partner = this.partners[i];
+         }
+     }
 })
+
+var stripHtmlFunction = function (value) {
+  var span = document.createElement("span");
+  span.innerHTML = value;
+  var text = span.textContent || span.innerText || "";
+  return text;
+};
+
+var truncateFunction = function(text, length, clamp) {
+    clamp = clamp || '...';
+    var node = document.createElement('div');
+    node.innerHTML = text;
+    var content = node.textContent;
+    return content.length > length ? content.slice(0, length) + clamp : content;
+};
+
+Vue.filter('truncate', truncateFunction);
+Vue.filter('striphtml', stripHtmlFunction)
